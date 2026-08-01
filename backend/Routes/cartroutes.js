e5c6cc5cc9017c85a43cb5eebb4e81173f77c9a5
Muhammad Haris <sheikharis574@gmail.com>
2026-08-01 14:59:36 +0500

    Created API cart Routes

---
+++ backend/Routes/cartroutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  addToCart,
+  removeItem,
+  updateQuantity,
+  getCart,
+} from "../controllers/cartController";
+
+const router = express.Router();
+
+router.post("/add", addToCart);
+router.post("/remove", removeItem);
+router.post("/update", updateQuantity);
+router.get("/:userId", getCart);
+
+export default router;
