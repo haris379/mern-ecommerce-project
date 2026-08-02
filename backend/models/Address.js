2866c27fcfa4743749baca2445b5dc25c521beb3
Muhammad Haris <sheikharis574@gmail.com>
2026-08-03 00:01:01 +0500

    Add Address model

---
+++ backend/models/Address.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const addressSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    fullName: String,
+    phone: String,
+    addressLine: String,
+    city: String,
+    state: String,
+    pincode: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+export default mongoose.model("Address", addressSchema);
